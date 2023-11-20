87b01273e748c8dd671b65015d94702fd5ba26b2
Matias Lago Evia <matiaslagoevia@gmail.com>
2023-11-20 18:38:48 -0500

    add color palette
    inspired by 80s miami/vice city - thanks chatGPT :)

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        "turquoise-blue": "#50D8D8",
+        "hot-pink": "#FF6EC7",
+        lavender: "#B98EFF",
+        "mint-green": "#7FFF7F",
+        "coral-orange": "#FF8C6B",
+        "electric-yellow": "#FFFF00",
+        "light-gray": "#D3D3D3",
+        white: "#FFFFFF",
+      },
+    },
+  },
+  plugins: [],
+};
